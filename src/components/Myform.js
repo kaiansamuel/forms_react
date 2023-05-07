88c8a5cb67e6326c91be64b2ce4e9769761ec764
Kaian Samuel <kaiansamuelster@gmail.com>
2023-05-07 11:41:40 -0300

    Enventos no input

--- src/components/Myform.js
+++ src/components/Myform.js
@@ -1,13 +1,22 @@
 import './Myform.css'
+import {useState} from 'react'
 
 const Myform = () => {
+
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleNome = (e) => {
+    setNome(e.target.value)
+  }
+  console.log(nome)
   return (
     <div>
       {/* 1 - Criação de form*/}
       <form>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" placeholder="Digite o seu nome"/> 
+          <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleNome}/> 
         </div>
         {/* 2 - Label envolvendo o input */}
         <label>
